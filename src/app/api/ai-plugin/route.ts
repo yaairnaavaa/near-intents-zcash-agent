1addee50cc32f2348adccf3732cd9dff77a5b1c0
Yair Nava <iryanavahe@ittepic.edu.mx>
2025-03-25 11:44:09 -0700

    feat: change server url

--- src/app/api/ai-plugin/route.ts
+++ src/app/api/ai-plugin/route.ts
@@ -11,7 +11,7 @@ export async function GET() {
         },
         servers: [
             {
-                url: "",
+                url: "https://near-intents-zcash-agent.vercel.app/",
             },
         ],
         "x-mb": {
